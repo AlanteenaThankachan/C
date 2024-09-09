4e99c3a0734b26160d2f06eaaa39f1bcf36cb2f3
Alanteena Thankachan <thankachanaf1972@gmail.com>
2024-09-09 15:59:31 +0530

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -1,91 +1,85 @@
 #include <stdio.h>
-void enqueue();
-void dequeue();
-void disply();
-int front=-1,rear=-1,m,item;
-int a[100];
-void main()
-{
-   int n;
-   printf("Enter the size of queue=");
-   scanf("%d",&m);
-   printf("Enter 1 for enqueue operation\n");
-   printf("      2 for dequeue operation\n");
-   printf("      3 for disply queue elements\n");
-   printf("      4  for stop the program\n");
-   while(n!=4)
-   {
-      printf("Enter the choice=");
-      scanf("%d",&n);
-      switch(n)
-      {
-          case 1:enqueue();
-                 break;
-          case 2:dequeue();
-                 break;
-          case 3:disply();
-                 break;
-          case 4:printf("End of program");
-                 break;
-      }
-   }
-   
-}
-void enqueue()
-{
-    if(front==((rear+1)%m))
-    {
-        printf("overflow\n");
-    }
-    else if(front==-1 && rear==-1)
-    {
-        front++;
-        rear++;
-        printf("Enter the value=");
-        scanf("%d",&a[rear]);
-    }
-    else
-    {
-        rear=(rear+1)%m;
-        printf("Enter the value=\n");
-        scanf("%d",&a[rear]);
-    }
-}
-void dequeue()
-{
-    if(front==-1 && rear==-1)
-    {
-        printf("underflow\n");
-    }
-    else if(front==rear)
-    {
-        item=a[front];
-        front=-1;
-        rear=-1;
-        printf("The deleded value is %d\n ",item);
-    }
-    else
-    {
-        item=a[front];
-        front=(front+1)%m;
-        printf("The deleted value is %d",item);
-    }
-   
-}
-void disply()
-{    
-    if(front==-1 && rear==-1)
-    {
-        printf("The queue is empty");
-    }
-    else
-    {
-       for(int i=front;i<=rear;i++)
-    {
-        printf("%d\t",a[i]);
-    }
-    }
-   
-}
-	
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+const int MAX = 100;
+
+char stack [100];
+int top = -1;
+
+void push(char c) {
+    if (top == MAX - 1) {
+        printf("Stack overflow\n");
+        exit(1);
+    }
+    stack[++top] = c;
+}
+
+char pop() {
+    if (top == -1) {
+        printf("Stack underflow\n");
+        exit(1);
+    }
+    return stack[top--];
+}
+
+int precedence(char c) {
+    if (c == '+' || c == '-') {
+        return 1;
+    } else if (c == '*' || c == '/') {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+void infixToPostfix(char *infix, char *postfix) 
+{
+    int i, j = 0;
+
+    for (i = 0; i < strlen(infix); i++) 
+    {
+        if (isspace(infix[i])) 
+	{
+            continue;
+        }
+
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        } else if (infix[i] == '(') {
+            push(infix[i]);
+        } else if (infix[i] == ')') {
+            while (stack[top] != '(') {
+                postfix[j++] = pop();
+            }
+            pop();
+        } else {
+            while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+    }
+
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("Enter infix expression: ");
+    fgets(infix, MAX, stdin);
+    infix[strcspn(infix, "\n")] = 0; 
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
+
+	
