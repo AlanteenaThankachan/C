2a7a58e4c6b922c4e49536fd4f54edcebd2a8755
Alanteena Thankachan <thankachanaf1972@gmail.com>
2024-09-09 16:22:09 +0530

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -3,82 +3,99 @@
 #include <string.h>
 #include <ctype.h>
 
-const int MAX = 100;
+const int MAX=100;
 
 char stack [100];
-int top = -1;
+int top=-1;
 
-void push(char c) {
-    if (top == MAX - 1) {
+void push(char c) 
+{
+    if (top==MAX-1) 
+    {
         printf("Stack overflow\n");
         exit(1);
     }
-    stack[++top] = c;
+    stack[++top]=c;
 }
 
-char pop() {
-    if (top == -1) {
+char pop()
+ {
+    if (top==-1) 
+    {
         printf("Stack underflow\n");
         exit(1);
     }
     return stack[top--];
 }
 
-int precedence(char c) {
-    if (c == '+' || c == '-') {
+int precedence(char c) 
+{
+    if (c=='+'||c=='-') 
+    {
         return 1;
-    } else if (c == '*' || c == '/') {
+    }
+     else if (c=='*'||c=='/') 
+    {
         return 2;
-    } else {
+    } 
+    else 
+    {
         return 0;
     }
 }
 
-void infixToPostfix(char *infix, char *postfix) 
-{
-    int i, j = 0;
+void infixToPostfix(char *infix, char *postfix)
+ {
+    int i,j=0;
 
-    for (i = 0; i < strlen(infix); i++) 
+    for (i=0;i<strlen(infix);i++) 
     {
         if (isspace(infix[i])) 
-	{
+        {
             continue;
         }
 
-        if (isalnum(infix[i])) {
+        if (isalnum(infix[i]))
+         {
             postfix[j++] = infix[i];
-        } else if (infix[i] == '(') {
+        } 
+        else if (infix[i]=='(') 
+        {
             push(infix[i]);
-        } else if (infix[i] == ')') {
-            while (stack[top] != '(') {
-                postfix[j++] = pop();
+        } 
+        else if (infix[i]==')')
+         {
+            while (stack[top]!='(')
+            {
+                postfix[j++]=pop();
             }
-            pop();
-        } else {
-            while (top != -1 && precedence(stack[top]) >= precedence(infix[i])) {
-                postfix[j++] = pop();
+            pop(); 
+        }
+         else 
+        {
+            while (top!=-1&& precedence(stack[top])>= precedence(infix[i])) 
+            {
+                postfix[j++]=pop();
             }
             push(infix[i]);
         }
     }
 
-    while (top != -1) {
-        postfix[j++] = pop();
+    while (top!=-1)
+     {
+        postfix[j++]=pop();
     }
-    postfix[j] = '\0';
+    postfix[j]='\0';
 }
 
-int main() {
-    char infix[MAX], postfix[MAX];
-
+int main() 
+{
+    char infix[MAX],postfix[MAX];
     printf("Enter infix expression: ");
-    fgets(infix, MAX, stdin);
-    infix[strcspn(infix, "\n")] = 0; 
-
-    infixToPostfix(infix, postfix);
-
-    printf("Postfix expression: %s\n", postfix);
-
+    fgets(infix,MAX,stdin);
+    infix[strcspn(infix,"\n")] =0; 
+    infixToPostfix(infix,postfix);
+    printf("Postfix expression:%s\n",postfix);
     return 0;
 }
 
